8ae25968d887d845d95555567a0dbc19ca59a899
JoseJaet <jose.escobar@ucsp.edu.pe>
2024-07-26 10:30:12 -0500

    Se incluyó en el main individual la función eliminar_filas_erroneas

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import tratamientoDx as ttdx
 
 df = pd.read_csv("Credit Card Clients.csv")
 
-print(exdx.matriz_correlación(df))
+#Tratamiento de datos
+df_col_limpio = ttdx.eliminar_columnas_erroneas(df)
+df_fil_limpio = ttdx.eliminas_filas_erroneas(df_col_limpio)
 
-print (ttdx.eliminar_columnas_erroneas(df))
+print(exdx.matriz_correlación(df_fil_limpio))
