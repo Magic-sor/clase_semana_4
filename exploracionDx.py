29cae8a967e44c3ecdbcfed5058c2a3271a87685
Cabad <carlosusil2014@gmail.com>
2024-07-26 10:20:47 -0500

    Se agrego la funcion create_pairplot en exploración Dx

--- exploracionDx.py
+++ exploracionDx.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import seaborn as sns
 
 def matriz_correlación(df):
     df_number = df.select_dtypes(include=['number'])
     correlacion = df_number.corr()
     return correlacion
+
+def create_pairplot(df):
+    df_number = df.select_dtypes(include=['number'])
+    return(sns.pairplot(df_number))
+    
